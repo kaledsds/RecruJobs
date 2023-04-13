e9d8131491ee2d8487861287f8396b237af60aa8
kaledsds <doiybikaled.com>
2023-04-13 21:15:01 +0000

    Feat:main page navbar

---
+++ src/config/main-page-config.ts
@@ -0,0 +1,27 @@
+export const navItem = [
+  {
+    id: "1",
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: "2",
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: "3",
+    name: "Services",
+    href: "#services",
+  },
+  {
+    id: "4",
+    name: "HIW",
+    href: "#hiw",
+  },
+  {
+    id: "5",
+    name: "Contact",
+    href: "#contact",
+  },
+];
---
+++ src/layouts/main-layout.tsx
@@ -0,0 +1,9 @@
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default MainLayout;
